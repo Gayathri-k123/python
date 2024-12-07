b51e2d9108eea54f04ba18639c153a200fa104da
Gayathri-k123 <gayathri39497@gmail.com>
2024-12-07 14:51:58 +0530

    Create exp8.py

---
+++ lc4/exp8.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter the first number:")) 
+n2=int(input("Enter the second number:")) 
+n3=int(input("Enter the third number:")) 
+if n1>n2 and n1>n3: 
+print(f"{n1} is the biggest") 
+elif n2>n3: 
+ 	print(f"{n2}is the biggest") 
+else: 
+ 	print(f"{n3} is the biggest") 
