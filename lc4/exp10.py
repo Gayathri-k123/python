1d54fc5521c108c9682973ac7a35203f704f74da
Gayathri-k123 <gayathri39497@gmail.com>
2024-12-07 14:53:16 +0530

    Create exp10.py

---
+++ lc4/exp10.py
@@ -0,0 +1,10 @@
+age=int(input("Enter the age:")) 
+if age<10: 
+print("Rate is:7") 
+elif age>=10 and age<60: 
+ 	print("Rate is:10") 
+elif age>=60: 
+ 	print("Rate is:5") 
+else: 
+ 	print("Invalid age") 
+
