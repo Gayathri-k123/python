52077747a7a525764894df52fcdcf3a339369d49
Gayathri-k123 <gayathri39497@gmail.com>
2024-12-07 15:22:00 +0530

    Create exp11.py

---
+++ lc3/exp11.py
@@ -0,0 +1,14 @@
+num=int(input("\nEnter a number:"))
+num_str=str(num)
+noofdigit=len(num_str)
+sum=0
+i=0
+while i<noofdigit:
+        n=int(num_str[i])
+        p=n**noofdigit
+        sum=sum+p
+        i=i+1
+if num==sum:
+        print(f"{num} is an Armstrong number\n")
+else:
+        print(f"{num} is not an Armstrong number\n")
