78a17ba06cffee7f20fff94f56717e4313e4544c
Gayathri-k123 <gayathri39497@gmail.com>
2024-12-18 14:47:54 +0530

    Add files via upload

---
+++ lc7/product.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,product=0;
+	printf("Enter the numbers");
+	scanf("%d%d",&a,&b);
+	product=a*b;
+	printf("Product of the given number is %d",product);
+	return 0;
+}
